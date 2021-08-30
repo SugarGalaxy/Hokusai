e98b9288e388356355399bb11cd941fa3db10f2e
Alen <alen.djonko@gmail.com>
2021-08-30 11:49:49 +0200

    Adjusted home_page.dart

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hokusai/pages/home_page/iob_chart.dart';
 import 'package:hokusai/pages/home_page/predicted_glucose_chart.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,10 +9,16 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height / 2,
-        child: PredictedGlucoseChart(),
-      ),
+      body: ListView(children: [
+        Container(
+          height: MediaQuery.of(context).size.height / 2,
+          child: PredictedGlucoseChart(),
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height / 2,
+          child: InsulinOnBoard(),
+        ),
+      ]),
     );
   }
 }
